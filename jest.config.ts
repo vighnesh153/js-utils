0dc96ede2096bcbf836a3aeee8d1354a6c37a7bc
Vighnesh Raut (vighnesh153) <vighnesh.raut13@gmail.com>
2022-01-31 09:46:00 +0530

    chore: add utils in coverageIgnore

--- jest.config.ts
+++ jest.config.ts
@@ -28,7 +28,8 @@ export default {
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
     "/node_modules/",
-    "index.ts$"
+    "index.ts$",
+    "/src/utils.ts"
   ],
 
   // Indicates which provider should be used to instrument code for coverage
