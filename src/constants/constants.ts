ea4a771e0eb045198448aebb29e05ca23d37cecd
Vighnesh Raut (vighnesh153) <vighnesh.raut13@gmail.com>
2022-02-01 23:30:28 +0530

    feat: added some useful constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,57 @@
+/**
+ * Some utility constants
+ */
+export abstract class Constants {
+  /**
+   * All the digits from 0 to 9
+   */
+  static digits = '0123456789';
+
+  /**
+   * All the digits for a hex-representation of a number
+   */
+  static hexDigits = '0123456789abcdefABCDEF';
+
+  /**
+   * All the digits for an octal-representation of a number
+   */
+  static octalDigits = '01234567';
+
+  /**
+   * All whitespace characters
+   */
+  static whitespace = ' \t';
+
+  /**
+   * All lowercase english alphabet letters
+   */
+  static lowercaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+  /**
+   * All uppercase english alphabet letters
+   */
+  static uppercaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  /**
+   * All english alphabet letters (both lowercase and uppercase)
+   */
+  static alphabet = Constants.lowercaseAlphabet + Constants.uppercaseAlphabet;
+
+  /**
+   * All punctuation characters
+   */
+  static punctuation = `!"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~`;
+
+  /**
+   * All the characters that can be printed
+   */
+  static printableCharacters =
+    Constants.digits +
+    Constants.alphabet +
+    Constants.punctuation +
+    Constants.whitespace;
+
+  protected constructor() {
+    throw new Error(`Don't instantiate or inherit the Constants class`);
+  }
+}
--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,4 @@ export * from './data_structures';
 export * from './math';
 export * from './helpers';
 export * from './algorithms';
+export * from './constants';
