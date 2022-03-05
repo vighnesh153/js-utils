6f6e8233235edb18f290ff13a11ed2c1c78a86d5
Vighnesh Raut (vighnesh153) <vighnesh.raut13@gmail.com>
2022-03-06 01:02:40 +0530

    docs: add docs for deepFreeze

--- src/helpers/deep_freeze/deep_freeze.ts
+++ src/helpers/deep_freeze/deep_freeze.ts
@@ -1,5 +1,10 @@
 import { DeepReadonly } from 'ts-essentials';
 
+/**
+ * Deep freezes the object passed to it
+ *
+ * @param o - object to be frozen
+ */
 export const deepFreeze = <T>(o: T): DeepReadonly<T> => {
   Object.values(o).forEach((v) => Object.isFrozen(v) || deepFreeze(v));
   return Object.freeze(o) as DeepReadonly<T>;
