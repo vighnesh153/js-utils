2da1ed0be0f7659d936ac153cc01b88f56fbe118
Vighnesh Raut (vighnesh153) <vighnesh.raut13@gmail.com>
2022-03-05 12:09:14 +0530

    refactor: simplify shuffle logic

--- src/math/random/shuffle/shuffle.ts
+++ src/math/random/shuffle/shuffle.ts
@@ -1,19 +1,8 @@
-import { randomInteger, range } from '@utils';
-
 /**
  * Shuffle an iterable
  *
  * @param iterable - array or string to be shuffled
  */
 export const shuffle = <T>(iterable: Iterable<T>): T[] => {
-  const array = Array.from(iterable);
-  const iterableLength = array.length;
-
-  // For each index, swap itself with any random index lower than itself
-  Array.from(range(iterableLength - 1, 0, -1)).forEach((index) => {
-    const swapIndex = index === 0 ? 0 : randomInteger(0, index - 1, 1);
-    [array[index], array[swapIndex]] = [array[swapIndex], array[index]];
-  });
-
-  return array;
+  return Array.from(iterable).sort(() => (Math.random() < 0.5 ? -1 : 1));
 };
